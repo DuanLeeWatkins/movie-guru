efa13e34ef832d766f1f688e9828c43af6312a9c
DuanLeeWatkins <leeduan19@gmail.com>
2022-01-07 13:04:16 -0500

    added main files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Styles
+
+import { GlobalStyle } from "./GlobalStyle";
+
+const App = () => {
+  return (
+    <div className="App">
+      Start here.
+      <GlobalStyle />
+    </div>
+  );
+};
+
+export default App;
