10d9e98ac4a06e8945bf8e06e3e7e4e6004dc512
DuanLeeWatkins <leeduan19@gmail.com>
2022-01-07 20:18:53 -0500

    created searchbar

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -4,16 +4,19 @@ import { useState, useEffect } from "react";
 import API from "../API";
 
 const initialState = {
-    page: 0,
-    results: [],
-    total_pages: 0,
-    total_results: 0
-}
+  page: 0,
+  results: [],
+  total_pages: 0,
+  total_results: 0,
+};
 
 export const useHomeFetch = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   const [state, setState] = useState(initialState);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+
+  
 
   const fetchMovies = async (page, searchTerm = "") => {
     try {
@@ -37,5 +40,5 @@ export const useHomeFetch = () => {
     fetchMovies(1);
   }, []);
 
-  return { state, loading, error };
+  return { state, loading, error, setSearchTerm };
 };
